d6e26d9c28b90d30c871077ab3ac5bc4cef68c00
sarahKasih <tm_k79@yahoo.com>
2020-07-22 23:18:08 +0300

    part1 gunt

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+'use strict';
+
+ module.exports = function (grunt) {
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // Automatically load required Grunt tasks
+    require('jit-grunt')(grunt, {
+        pluginsRoot: 'other/dir',
+        sprite: 'grunt-spritesmith',
+        foo: '@abc/grunt-foo',        // for private modules.
+        bar: 'custom/bar.js'          // for custom tasks.
+      });
+
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+        sass: {
+            dist: {
+                files: {
+                    'css/styles.css': 'css/styles.scss'
+                }
+            }
+        },
+        watch: {
+            files: 'css/*.scss',
+            tasks: ['sass']
+        },
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src : [
+                        'css/*.css',
+                        '*.html',
+                        'js/*.js'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: "./"
+                    }
+                }
+            }
+        }
+
+    });
+
+    grunt.registerTask('css', ['sass']);
+    grunt.registerTask('default', ['browserSync', 'watch']);
+
+};
